e538de86be385a042d2cf4a58f1d27f4b2d80106
DWang5090 <davidcxwang@hotmail.com>
2014-04-21 15:19:45 -1000

    Changes

---
+++ matrix.js
@@ -0,0 +1,25 @@
+function searchCharacters(chars) {
+	var characterMatrixArray = {};
+	var nodesArray = new Array();
+	var linksArray = new Array();
+	$.getJSON("./characterMatrix.js", function(data) {		
+		for (var i=0; i<chars.length; i++) {
+			//console.log(i);
+			//console.log(chars[i]);
+			nodesArray.push({"node":i, "name":chars[i]});	
+			for (var j=0; j<chars.length; j++) {
+				if (i<j) {
+					if (typeof(data[chars[i]][chars[j]]) != "undefined") {
+						linksArray.push({"source":i, "target":j, "value":data[chars[i]][chars[j]]});
+					}
+				}
+			}
+
+		}
+
+
+	});
+	characterMatrixArray['nodes'] = nodesArray;
+	characterMatrixArray['links'] = linksArray;
+	console.log(characterMatrixArray);
+}
